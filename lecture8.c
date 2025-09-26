0ebe743f511201026454542781df0ce83bd51832
areesha-git <sheikhareesha04@gmail.com>
2025-09-26 13:24:41 +0530

    Add files via upload

---
+++ lecture8.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main (){
+   /*for (int i = 0; i < 3; i++){
+    
+        for (int j = 0; j < 5; j++)
+        {
+            printf("%d ",j);
+        }
+        
+    }*/
+
+   for (int i = 0; i < 3; i++){
+    
+        for (int j = 0; j < 3 ; j++)
+        {
+            printf("* ");
+        }
+         printf(" \n");
+    }
+   
+}

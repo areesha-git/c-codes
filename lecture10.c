fa2c1cb17ce26e6ab2f9ed944cd9d3d88bb6d258
areesha-git <sheikhareesha04@gmail.com>
2025-09-26 13:41:54 +0530

    Add files via upload

---
+++ lecture10.c
@@ -0,0 +1,107 @@
+/*#include <stdio.h>
+
+int main()
+{
+    for(int i=0;i<4;i++){
+
+     for(int p=0; p<=i ;p++){
+         printf("*");
+     }
+
+
+ for(int j=3;j>=i; j--){
+     printf(" ");
+ }
+
+ for(int j=2;j>=i; j--){
+     printf(" ");
+ }
+
+ for(int k=0;k<=i;k++){
+     printf("*");
+ }printf("\n");
+  }
+ }
+
+#include<stdio.h>
+int main() {
+    int n=5;
+    for(int i=1;i<=n;i++) {
+        for(int j=1;j<=n;j++) {
+            if(i==1 || i==n || j==1 || j==n) {
+                printf("* ");
+            } else
+            {
+                printf("  ");
+            }
+            
+        }
+        printf("\n");
+    }
+#include<stdio.h>
+int main() {
+    int n=5;
+    for(int i=1;i<=n;i++) {
+        for(int j=1;j<=n;j++) {
+            if(i==1 || i==n || j==1 || j==n) {
+                printf("* ");
+            } else
+            {
+                printf("  ");
+            }
+            
+        }
+        printf("\n");
+    }
+
+
+} */
+/*#include<stdio.h>
+int main() {
+    int n=5;
+    for(int i=1;i<=n;i++) {
+        for(int j=1;j<=n;j++) {
+            if(i==1 || i==n || j==1 || j==n) {
+                printf("* ");
+            } else
+            {
+                printf("  ");
+            }
+            
+        }
+        printf("\n");
+    }
+}*/
+
+
+#include<stdio.h>
+int main(){
+    for(int i=0;i<4;i++){
+        for(int j=0; j<=i;j++){
+            printf("*");
+        
+        }
+        for (int k = 4; k >i; k--)
+        {
+            printf("  ");}
+        for(int j=0; j<=i;j++){
+            printf("*");
+        } 
+        // printf("\n");
+        printf("\n");
+    }
+    for (int i = 0; i < 4; i++)
+    {
+    for(int j=4; j>=i;j--){
+            printf("*");
+        
+        }
+        for (int k = 0; k <i; k++)
+        {
+            printf("  ");}
+        for(int j=4; j>=i;j--){
+            printf("*");
+        }
+        printf("\n");}
+        return 0;
+}

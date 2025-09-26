cf281827f30232e39bf568173a921103b45782f8
areesha-git <sheikhareesha04@gmail.com>
2025-09-26 13:25:10 +0530

    Add files via upload

---
+++ lecture8_pattern.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+ int main(){
+   /*for (int i = 0; i<3; i++){
+        for (int j = 0; j <=i ; j++)
+        {
+            printf("* ");
+        }
+         printf("\n");
+    }*/
+    
+/*for (int i = 0; i < 3; i++){
+     printf("");
+        for (int j = 3 ; j >i ; j--)
+        {
+            printf("* ");
+        }
+         printf("\n");
+    }*/
+   /*for (int i = 0; i < 3; i++){
+   
+        for (int j = 3 ; j >i ; j--)
+        {
+            printf(" ");
+        }
+        for (int k = 0; k <=i; k++)
+        {
+           printf("*");
+        }
+        
+         printf("\n");
+    }*/
+         
+     
+for (int i = 0; i < 3; i++){
+   
+        for (int j = 0; j <=i ; j++)
+        {
+            printf(" ");
+        }
+        for (int k =  3; k >i; k--)
+        {
+           printf("*");
+        }
+        
+         printf("\n");
+    }
+
+ }
